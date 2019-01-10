76fb59829fbfa246da1b8ad8fa0774d83d455481
godyn <godyn.ola@gmail.com>
2019-01-10 14:55:45 +0100

    Calculator - methods added.

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -2,8 +2,18 @@ package com.calculator;
 
 public class Calculator{
 
-    public static void main (String[] args){
+    private void addAtoB(int a, int b){
+        System.out.println("Add " + a + " to " + b + " equals " + (a+b));
+    }
 
+    private void subtractAfromB(int a, int b){
+        System.out.println("Subtract " + a + " from " + b + " equals " + (b-a));
+    }
+
+    public static void main (String[] args){
+        Calculator calc1 = new Calculator();
+        calc1.addAtoB(3, 5);
+        calc1.subtractAfromB(12, 40);
 
     }
 }
